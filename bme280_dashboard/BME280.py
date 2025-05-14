f0d2c704db1eab0c855342c8d3ab3b38a1b0e6bf
Anass Acheffay <anass.acheffay@obi.de>
2025-05-14 14:06:57 +0200

    TESTING

--- bme280_dashboard/BME280.py
+++ bme280_dashboard/BME280.py
@@ -1,56 +1,69 @@
 import threading
 import time
 
-SENSOR_DIR = "/sys/devices/platform/soc/fe804000.i2c/i2c-1/1-0077/iio:device0"
-UPDATE_SEC = 1.0
+FALLBACK_IIO_PATH = "/sys/devices/platform/soc/fe804000.i2c/i2c-1/1-0077/iio:device0/"
+HUMIDITY_INPUT_FILE = "in_humidityrelative_input"
+PRESSURE_INPUT_FILE = "in_pressure_input"
+TEMPERATURE_INPUT_FILE = "in_temp_input"
 
-class Sensor:
-    def __init__(self, name, fname):
-        self.name = name
-        self.fname = fname
-        self.value = None
+UPDATE_INTERVAL = 1.0
 
-    def read(self, base):
-        try:
-            with open(f"{base}/{self.fname}") as f:
-                raw = f.read().strip()
-            self.value = float(raw) / 1000.0
-        except:
-            self.value = None
 
-class Monitor:
-    def __init__(self, path=None):
-        self.path = path or SENSOR_DIR
-        self.sensors = [
-            Sensor('temp', 'in_temp_input'),
-            Sensor('hum', 'in_humidityrelative_input'),
-            Sensor('pres', 'in_pressure_input')
-        ]
-        self._stop = False
-        self.thread = threading.Thread(target=self._run)
-        for s in self.sensors:
-            s.read(self.path)
-        self.thread.start()
+class BME280Sensor:
 
-    def _run(self):
-        while not self._stop:
-            for s in self.sensors:
-                s.read(self.path)
-            time.sleep(UPDATE_SEC)
+    def __init__(self, value, input_file):
+        self.value: float | None = value 
+        self.input_file = input_file
+
+
+class BME280:
+
+    def __init__(self, iio_path=None):
+        self.__iio_path = iio_path or FALLBACK_IIO_PATH
+        self.__update_interval = UPDATE_INTERVAL
+        self.__stop_event = threading.Event()
+        self.__thread = threading.Thread(target=self.__update_loop, daemon=True)
+
+        self.sensors = {
+            "temperature": BME280Sensor(None, TEMPERATURE_INPUT_FILE),
+            "humidity": BME280Sensor(None, HUMIDITY_INPUT_FILE),
+            "pressure": BME280Sensor(None, PRESSURE_INPUT_FILE),
+        }
+
+        for sensor_name in self.sensors.keys():
+            self.__update_sensor_value(sensor_name)
+
+        self.__thread.start()
 
     def stop(self):
-        self._stop = True
-        self.thread.join()
+            self.__stop_event.set()
+            self.__thread.join()
 
-    def latest(self):
-        return {s.name: s.value for s in self.sensors}
 
-if __name__ == '__main__':
-    m = Monitor()
-    try:
-        while True:
-            d = m.latest()
-            print(f"Temp={d['temp']}, Hum={d['hum']}, Pres={d['pres']}")
-            time.sleep(5)
-    except KeyboardInterrupt:
-        m.stop()
+    def __update_loop(self):
+        while not self.__stop_event.is_set():
+            for sensor_name in self.sensors:
+                self.__update_sensor_value(sensor_name)
+            time.sleep(self.__update_interval)
+
+
+    def __update_sensor_value(self, sensor_name):
+        sensor = self.sensors[sensor_name]
+
+        try:
+            sensor.value = self.__string_input_to_float(
+                self.__read_file(f"{self.__iio_path}{sensor.input_file}")
+            )
+        except:
+            sensor.value = None
+
+
+    @staticmethod
+    def __string_input_to_float(string_input):
+        return float(string_input) / 1000. if string_input is not None else None
+
+
+    @staticmethod
+    def __read_file(filename):
+        with open(filename, 'r') as f:
+            return f.read().strip()
